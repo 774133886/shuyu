9ded675413a8f4f3dd38d909008bfa30c5aea7b5
Chris-Clear <602294138@qq.com>
2019-01-25 20:39:13 +0800

    1252039

---
+++ 小程序HTML/pages/paySuccess/paySuccess.js
@@ -0,0 +1,94 @@
+// pages/paySuccess/paySuccess.js
+var http = require('../../http.js')
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info: {},
+    days: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 获取数据
+   
+  },
+  doneBtn:function(){
+    wx.navigateBack();
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    var that = this;
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    var user = wx.getStorageSync('user');
+    return {
+      title: '跟我一起重新认识一本书',
+      path: '/pages/index/index?pid=' + user.id,
+      imageUrl: '/files/icon_book.png',
+      success: (res) => {    // 成功后要做的事情
+        //console.log(res.shareTickets[0])
+        // console.log
+        that.setData({
+          signBoxShow: false
+        })
+      },
+      fail: function (res) {
+        // 分享失败
+        console.log(res)
+      }
+    }
+
+  }
+})
